f28bd905aab48e311ad3dcfe8d3c6ab2c50c98f8
etipsin <e.tipsin@mail.ru>
2021-10-03 11:58:29 +0300

    Update config.py

--- backend/backend/config.py
+++ backend/backend/config.py
@@ -16,8 +16,7 @@ class Config:
 
     # postgres
     POSTGRES_DSN = en(
-        "DATABASE_URL",
-        "postgresql://dwwnedmbkjlrmu:73dbd4f4ac5b78d3209768a2b92dde8f8c9d1d8b1bc5c45f63541bdc094c5d35@ec2-54-73-58-75.eu-west-1.compute.amazonaws.com:5432/ddh32q28b5vmav"
+        "DATABASE_URL"
     )
     POSTGRES_MIN_SIZE = "5"
     POSTGRES_MAX_SIZE = "75"
